92799cda0c203475ceab8798c44e04db7d6fc6fe
Joongho <honor9320@gmail.com>
2026-09-10 12:41:47 +0900

    9/10

---
+++ Python/02-5_딕셔너리_자료형.py
@@ -0,0 +1,41 @@
+# dic = {"name": "pey", "phone": "010-1234-5678", "birth": "1118"}
+# print(dic)
+
+# a = {1: "hi"}
+# print(a)
+
+# a = {"a": [1, 2, 3]}
+# print(a)
+
+# a = {1: "a"}
+# a[2] = "b"
+# print(a)
+
+# a["name"] = "pey"
+# print(a)
+
+# a[3] = [1, 2, 3]
+# print(a)
+
+# del a[1]
+# print(a)
+
+# del a["name"]
+# print(a)
+
+# grade = {"pey": 10, "julliet": 99}
+# print(grade["pey"])
+# print(grade["julliet"])
+
+# a = {1: "a", 2: "b"}
+# print(a[1])
+# print(a[2])
+
+# a = {"a": 1, "b": 2}
+# print(a["a"])
+# print(a["b"])
+
+# dic = {"name": "pey", "phone": "010-1234-5678", "birth": "1111"}
+# print(dic["name"])
+# print(dic["phone"])
+# print(dic["birth"])
